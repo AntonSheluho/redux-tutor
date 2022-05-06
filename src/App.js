956c8915ffeb25a40bd77448b957d8c904dcc388
AntonSheluho <grinfrend2@gmail.com>
2022-05-06 10:50:47 +0300

    hook up git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cashReducer.cash) ;
+  const customer = useSelector(state => state.customerReducer.customer);
+  function addCustomer() {
+    const customer = {
+      name: prompt('type your name'),
+      id: Date.now(),
+    }
+    dispatch({type: 'ADD_CUSTOMER', payload: customer})
+  }
+  function removeCustomer(customer) {
+    dispatch({type: 'REMOVE_CUSTOMER', payload: customer.id})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{fontSize: '2rem'}} >{cash}</div>
+      <div style={{display:'flex'}} >
+
+        <button onClick={() => {
+          let sum = +prompt('enter sum')
+          dispatch({type: 'ADD_CASH', payload: sum});
+        }} >Add cash</button>
+
+        <button onClick={() => {
+          let sum = +prompt('enter sum')
+          dispatch({type: 'GET_CASH', payload: sum});
+        }} >Get cash</button>
+        <button onClick={addCustomer} >Add customer</button>
+        <button  >Remove customer</button>
+
+      </div>
+      {customer.length > 0 
+        ? <div>{customer.map(cus => 
+            <div onClick={() => removeCustomer(cus)} >{cus.name}</div>
+          )}</div>
+        : <div>There aren't any customers</div>
+      }
+      
+
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers } from 'redux';
+import { cashReducer } from './cashReducer';
+import { customerReducer } from './customerReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+const rootReducer = combineReducers({
+    customerReducer,
+    cashReducer
+})
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export {
+    store
+}
