eea63ad3b483a5812c0d22c01a5b9b4245b05f1f
AntonSheluho <grinfrend2@gmail.com>
2022-05-06 11:03:53 +0300

    cleaned App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
 
-  const dispatch = useDispatch();
-  const cash = useSelector(state => state.cashReducer.cash) ;
-  const customer = useSelector(state => state.customerReducer.customer);
-  function addCustomer() {
-    const customer = {
-      name: prompt('type your name'),
-      id: Date.now(),
-    }
-    dispatch({type: 'ADD_CUSTOMER', payload: customer})
-  }
-  function removeCustomer(customer) {
-    dispatch({type: 'REMOVE_CUSTOMER', payload: customer.id})
-  }
-
   return (
     <div className="App">
-      <div style={{fontSize: '2rem'}} >{cash}</div>
-      <div style={{display:'flex'}} >
-
-        <button onClick={() => {
-          let sum = +prompt('enter sum')
-          dispatch({type: 'ADD_CASH', payload: sum});
-        }} >Add cash</button>
-
-        <button onClick={() => {
-          let sum = +prompt('enter sum')
-          dispatch({type: 'GET_CASH', payload: sum});
-        }} >Get cash</button>
-        <button onClick={addCustomer} >Add customer</button>
-        <button  >Remove customer</button>
-
-      </div>
-      {customer.length > 0 
-        ? <div>{customer.map(cus => 
-            <div onClick={() => removeCustomer(cus)} >{cus.name}</div>
-          )}</div>
-        : <div>There aren't any customers</div>
-      }
-      
-
     </div>
   );
 }
